d49d10c56cabc89ca0431529c408464d88e983b3
Chamodi Thennakoon <58078417+chamodit@users.noreply.github.com>
2020-10-08 21:48:02 +0530

    Created profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { UserComponent } from './components/dashboard/user/user.component';
+import { AddUserComponent } from './components/dashboard/user/add-user/add-user.component';
+import { ManageUserComponent } from './components/dashboard/user/manage-user/manage-user.component';
+import { ProfilesComponent } from './components/dashboard/profiles/profiles.component';
+import { CreateProfileComponent } from './components/dashboard/profiles/create-profile/create-profile.component';
+import { ManageProfileComponent } from './components/dashboard/profiles/manage-profile/manage-profile.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, LoginComponent],
+  declarations: [AppComponent, DashboardComponent, LoginComponent, UserComponent, AddUserComponent, ManageUserComponent, ProfilesComponent, CreateProfileComponent, ManageProfileComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/dashboard/profiles/manage-profile/manage-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-profile',
+  templateUrl: './manage-profile.component.html',
+  styleUrls: ['./manage-profile.component.css']
+})
+export class ManageProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
