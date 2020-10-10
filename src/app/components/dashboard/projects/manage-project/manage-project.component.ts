d55bc2867f885a4ce6ce56ea5554d33b7ecad61a
Chamodi Thennakoon <58078417+chamodit@users.noreply.github.com>
2020-10-11 00:49:38 +0530

    Done some chnages in projects/sites managing

--- src/app/components/dashboard/projects/manage-project/manage-project.component.ts
+++ src/app/components/dashboard/projects/manage-project/manage-project.component.ts
@@ -6,65 +6,67 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatPaginator } from '@angular/material/paginator';
 
 interface APIResponse {
-  success : boolean,
-  data : any
+  success: boolean;
+  data: any;
 }
 
 @Component({
   selector: 'app-manage-project',
   templateUrl: './manage-project.component.html',
-  styleUrls: ['./manage-project.component.css']
+  styleUrls: ['./manage-project.component.css'],
 })
 export class ManageProjectComponent implements OnInit {
-
   displayedColumns = ['siteNo', 'siteName', 'action'];
   dataSource = new MatTableDataSource();
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
-  private _id: String;
-  private siteNo: String;
-  private siteName: String;
-  private address: String;
-  private city: String;
-  private phoneNo: Number;
 
   constructor(
     private siteService: SitesService,
     private snackBar: MatSnackBar,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this._id = '';
-    this.siteNo = '';
-    this.siteName = '';
-    this.address = '';
-    this.city = '';
-    this.phoneNo = this.phoneNo;
+   
     this.viewAllSites();
   }
 
   viewAllSites() {
-    this.siteService.viewSites().subscribe((response : APIResponse) => {
-        this.dataSource = response.data; 
+    this.siteService.viewSites().subscribe(
+      (res: any) => {
+        this.dataSource = res.data;
         this.dataSource.paginator = this.paginator;
+      },
+      (err) => {
+        console.log(err.message);
+      }
+    );
+    this.siteService.viewSites().subscribe((response: APIResponse) => {
+      this.dataSource = response.data;
+      this.dataSource.paginator = this.paginator;
     });
   }
-
+ 
   addSite() {
-    this.router.navigate(['dashboard/projects/create-project'],);
+    this.router.navigate(['dashboard/projects/create']);
   }
 
-  deleteSiteDetails(id: String){
-    this.siteService.deleteSiteById(id).subscribe(response => {
-      console.log(response);
-      this.snackBar.open('Site Details Are Successfully Deleted', null, { duration : 2000 } );  
-      this.viewAllSites();
-    }, err => {
-      this.snackBar.open('Unsuccessful', null, { duration : 3000 } );
-      console.log(err.message);
-    });
+  deleteSiteDetails(id: String) {
+    this.siteService.deleteSiteById(id).subscribe(
+      (response) => {
+        console.log(response);
+        this.snackBar.open('Site Details Are Successfully Deleted', null, {
+          duration: 2000,
+        });
+        this.viewAllSites();
+      },
+      (err) => {
+        this.snackBar.open('Unsuccessful', null, { duration: 3000 });
+        console.log(err.message);
+      }
+    );
   }
-
 }
+
